e89b2dc58645d77c4b6f419c841c268d9c8ba6ad
Suraj kumar <90993575+Surajmicky@users.noreply.github.com>
2022-02-27 15:11:16 +0530

    added footer

---
+++ components/footer.js
@@ -0,0 +1,86 @@
+function footer(){
+    return` <footer class="footer" id="footer">
+    <div id="footer1">
+        <div id="footer1_1">
+            <p id="sizing">You’ll love us even more on the app!</p>
+            <br/>
+            <h1>Flat ₹200 OFF</h1>
+            <h2>on first app order</h2>
+            <div id="coupan">
+            
+                <p>Code: MEAL200  <i class="far fa-copy" id="copy"></i></p>
+               </div>
+            
+            <div id="playstore_img">
+                <img src="https://d2pc4443o2qatl.cloudfront.net/20220119101543/assets/images/app-store.png" alt="">
+                <img src="https://d2pc4443o2qatl.cloudfront.net/20220119101543/assets/images/google-play.png" alt="">
+            </div>
+        </div>
+        <div id="footer1_2">
+            <img src="https://d2pc4443o2qatl.cloudfront.net/20220112094354/assets/images/footer-phone-1.png" alt="">
+            <img src="https://d2pc4443o2qatl.cloudfront.net/20220112094354/assets/images/footer-phone-2.png" alt="">
+        </div>
+    </div>
+    
+    
+    
+            <div class="container">
+    
+                  
+                <div class="div3_3">
+                    <div class="div3">
+                        <img src="https://d2pc4443o2qatl.cloudfront.net/20220112094354/assets/images/brand_features/download-black.svg" alt="">
+                      <div>
+                          <h2>1M+</h2>
+                          <p>App Downloads</p>
+                      </div>
+                    </div>
+                    <div class="div3">
+                        <img src="https://d2pc4443o2qatl.cloudfront.net/20220112094354/assets/images/brand_features/outlet-black.svg" alt="">
+                        <div>
+                            <h2>120+ Outlets</h2>
+                            <p>In Bangalore, Delhi-NCR, Mumbai, Pune & Hyderabad</p>
+                        </div>
+                    </div>
+                    <div class="div3">
+                        <img src="https://d2pc4443o2qatl.cloudfront.net/20220112094354/assets/images/brand_features/customers-black.svg" alt="">
+                        <div>
+                            <h2>20L+</h2>
+                            <p>Satisfied Customers</p>
+                        </div>
+                    </div>
+                </div>
+                  <div class="row">
+                      <div class="footer-col">
+                        <h4>BOX8</h4>
+                        <ul>
+                            <li><a href="*">ABOUT US</a></li>
+                            <li><a href="*">HELP & SUPPORT</a></li>
+                            <li><a href="*">CONTACT US</a></li>
+                            <li><a href="*">DELIVERY POLICIES</a></li>
+                            <li><a href="*">PRIVACY POLICIES</a></li>
+                        </ul>
+                      </div>
+                      <div class="footer-col">
+                        <h4>EXPLORE</h4>
+                        <ul>
+                            <li><a href="*">OFFERS</a></li>
+                            <li><a href="*">BLOG</a></li>
+                            <li><a href="*">DISCLAIMERS</a></li>
+                            <li><a href="*">REFER & EARN</a></li>
+                        </ul>
+                      </div>
+                      <div class="footer-col">
+                        <h4>FOLLOW</h4>
+                        <ul id="social_media_icon">
+                           <li><i class="fab fa-facebook-f"></i></li> 
+                           <li><i class="fab fa-twitter"></i></li>
+                        </ul>
+                      </div>
+                 </div>
+                 <hr id="line">
+                 <p id="footerdone">Copyright 2013 - 2022. Poncho Hospitality Pvt. Ltd. All rights reserved.   v2.1.13</p>
+            </div>
+        </footer>`
+}
+export default footer
